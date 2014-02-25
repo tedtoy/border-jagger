5d26ada618f62eccf0b7da0f466772b034b175b2
teddy <teddy@thinkpad.(none)>
2014-02-24 21:42:42 -0800

    added jquery to use offset, height and width. added methods for finding overlapping elements and overlapping portions of an element

--- jagged.js
+++ jagged.js
@@ -39,31 +39,119 @@
 //       overlapping elements.
 // --------------------------------------------------------
 
-var Jagged = (function(){
+var Jagged = (function($){
 
-    // store the elements in the module
-    var els = document.getElementsByClassName(className);
-    
-    // Find overlapping elements:
-    function getOverlapping(){
-        for(i=0; i< els.length; i++){
-            
+    // For each element A, finds all overlapping elements
+    // and stores the resulting array as an attribute of A.
+    function getOverlappingElements(className){
+        var elems = document.getElementsByClassName(className);
+        var positionEls = [];
+        // Save positions in allPos:
+        for(var i=0; il=elems.length; i< il; i++){
+            var el = $(all[i]);
+            var elPos = el.offset();
+            positionEls.push({
+                'left':   elPos.left,
+                'right':  elPos.left + el.width(),
+                'top':    elPos.top,
+                'bottom': elPos.top + el.height(),
+                'overlapsWith': [],
+                // defined as vertical ranges:
+                'leftOverlaps': [],
+                'rightOverlaps': [],
+                'topOverlaps': [],
+                'bottomOverlaps': []
+            });
         }
+        // Determine overlaps. n^2 is the best I can do.
+        for(var a=0; al=positionEls.length; a< al; a++){
+            for(var b=0; b< al; b++){
+                if( comparePos(positionEls[a], positionEls[b]) ){
+                    a.overlapsWith.push(positionEls[b]);
+                }
+            }
+            getExposedBorders(a);
+        }
+        return positionEls;
+    }
+
+    function getExposedBorders(p){
+        // 1. First find the overlapping portions of p's edges:
+        for(var a=0; l = p.overlapsWith.length; a<l; a++){
+            var p2 = p.overlapsWith[a];
+            // horizontal overlapping range:
+            var overlapH = [
+                (p2.left <= p.left) ? 0 : p2.left - p1.left ,
+                (p2.right >= p.right) ? p.right - p.left : p2.right - p.left
+            ];
+            // vertical overlapping range:
+            var overlapV = [
+                (p2.top <= p.top) ? 0 : p2.top - p.top,
+                (p2.bottom >= p.bottom) ? p.bottom - p.top : p2.bottom - p.top
+            ];
+            // overlaps top?
+            if( isBetween(p.top, p2.top, p2.bottom) ){
+                p.topOverlaps.push(overlapH);
+            }
+            // overlaps bottom?
+            if( isBetween(p.bottom, p2.top, p2.bottom) ){
+                p.bottomOverlaps.push(overlapH);
+            }
+            // overlaps left side?
+            if( isBetween(p.left, p2.left, p2.right) ){
+                p.leftOverlaps.push(overlapV);
+            }
+            // overlaps right side?
+            if( isBetween(p.right, p2.left, p2.right) ){
+                p.rightOverlaps.push(overlapV);
+            }
+        }
+        // 2. Find the non-overlapping portions:
+        var allOverlaps = [p.topOverlaps, p.bottomOverlaps, p.leftOverlaps, p.rightOverlaps];
+        $.each( allOverlaps , function(idx, overlaps){
+            $.each( overlaps, function(idx2, o){
+                // saving work..
+            });
+        });
+    }
+    // Checks if integer x is between a and b
+    function isBetween(x,a,b){
+        if( (x >= a) && (x <= b) ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    
+
+    function comparePos(p1, p2){
+        // Determine if p1 overlaps p2
+        var overlap = false;
+        // overlap horizontally?
+        if( (p1.left >= p2.left) && (p1.left <= p2.right) ){
+            // vertically?
+            if( (p1.top >= p2.top) && (p1.top <= p2.bottom) ){
+                overlap = true;
+            }
+        }
+        return overlap;
     }
 
     return function(className){
         
-        this.overlappingEls = getOverlapping(className),
+        this.overlappingElements = getOverlappingElements(className);
         
 
         this.border = function(style){
-             
-        },
+             console.log("border");
+             console.log(style);
+        };
 
-        return this;
+        //return this;
     }
 
-}());
+}(jQuery));
 
 
 
